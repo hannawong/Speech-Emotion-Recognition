564dbc21c8519c79f763ed0d0ac9d7749cb2ad9c
hannawong <zh-wang17@mails.tsinghua.eedu.cn>
2022-02-28 04:51:50 +0800

    german ablation

--- allosaurus+CNN/SER/modeling/ser_model.py
+++ allosaurus+CNN/SER/modeling/ser_model.py
@@ -5,16 +5,34 @@ import math
 import numpy as np
 
 class SER_MODEL(nn.Module):
-    def __init__(self, audio_maxlen, num_labels, hidden_size = 64):
+    def __init__(self, args,audio_maxlen, num_labels, hidden_size = 64):
 
         super(SER_MODEL, self).__init__()
         self.audio_maxlen = audio_maxlen
         self.num_labels = num_labels
-
-        #########  for SER classification task   ##########
+        self.args = args
+        #########  Allosaurus feature   ##########
         self.conv1d_1 = nn.Conv1d(in_channels=230, out_channels=128, kernel_size=3,padding = "same")
         self.conv1d_2 = nn.Conv1d(in_channels=230, out_channels=128, kernel_size=5,padding = "same")
         self.lstm = nn.LSTM(128, 128, 1, bidirectional=True)
+        self.adaptor0 = nn.Linear(256,128)
+        self.Q_layer = nn.Linear(128,128)
+        self.K_layer = nn.Linear(128,128)
+        self.V_layer = nn.Linear(128,128)
+
+        ############# for MFCC ###############
+        self.conv1d_1_mfcc = nn.Conv1d(in_channels=24, out_channels=24, kernel_size=3,padding = "same")
+        self.conv1d_2_mfcc = nn.Conv1d(in_channels=24, out_channels=24, kernel_size=5,padding = "same")
+        self.lstm_mfcc = nn.LSTM(24, 64, 1, bidirectional=True)
+        self.Q_layer_mfcc = nn.Linear(128,128)
+        self.K_layer_mfcc = nn.Linear(128,128)
+        self.V_layer_mfcc = nn.Linear(128,128)
+
+        ########### for concat ##############
+        self.adaptor = nn.Linear(128,256)
+        self.adaptor1 = nn.Linear(128,256)
+
+        ########## MLP ############
         self.dense1 = nn.Linear(128*2, hidden_size*4)
         self.bn = torch.nn.BatchNorm1d(hidden_size*4)
         self.dropout = nn.Dropout(0.1)
@@ -22,16 +40,9 @@ class SER_MODEL(nn.Module):
         self.dense3 = nn.Linear(hidden_size,hidden_size // 2)
         self.out_proj = nn.Linear(hidden_size//2, num_labels)
 
-        ########### for self-attention layer ##############
-        self.adaptor0 = nn.Linear(256,128)
-        self.Q_layer = nn.Linear(128,128)
-        self.K_layer = nn.Linear(128,128)
-        self.V_layer = nn.Linear(128,128)
-        self.adaptor = nn.Linear(128,256)
-
-    def forward(self, feat_emb, ge2e_emb,label,length):
+    def forward(self, feat_emb, ge2e_emb,mfcc_emb,label,length,mfcc_length):
         
-        return self.classification_score(feat_emb,ge2e_emb,label,length)
+        return self.classification_score(feat_emb,ge2e_emb,mfcc_emb,label,length,mfcc_length)
 
 
     def get_attention_mask(self,lstm_output,length):
@@ -46,8 +57,6 @@ class SER_MODEL(nn.Module):
     def self_attention_layer(self,lstm_output,length):  # TODO: multi-head self-attention
         last_hidden_state = [lstm_output[i,length[i].long(),:] for i in range(lstm_output.shape[0])] ## the actual hidden state!
         last_hidden_state = torch.stack(last_hidden_state,axis = 0)
-        print(last_hidden_state.shape)
-
         Q_last_hidden_state = self.Q_layer(last_hidden_state) ##Query, (batchsize,128)
         Q_last_hidden_state = torch.unsqueeze(Q_last_hidden_state,1)
         K = self.K_layer(lstm_output) ##(batchsize,max_len,128)
@@ -66,9 +75,31 @@ class SER_MODEL(nn.Module):
         context_layer = torch.matmul(attention_probs, V)
         return context_layer.squeeze()
 
+    def self_attention_layer_mfcc(self,lstm_output,length):  # TODO: multi-head self-attention
+        last_hidden_state = [lstm_output[i,length[i].long(),:] for i in range(lstm_output.shape[0])] ## the actual hidden state!
+        last_hidden_state = torch.stack(last_hidden_state,axis = 0)
+        Q_last_hidden_state = self.Q_layer_mfcc(last_hidden_state) ##Query, (batchsize,128)
+        Q_last_hidden_state = torch.unsqueeze(Q_last_hidden_state,1)
+        K = self.K_layer_mfcc(lstm_output) ##(batchsize,max_len,128)
+        V = self.V_layer_mfcc(lstm_output) ##(batchsize,max_len,128)
+        attention_scores = torch.matmul(Q_last_hidden_state,K.permute(0,2,1))
+        attention_scores = torch.multiply(attention_scores,
+                                   1.0 / math.sqrt(float(attention_scores.shape[-1])))
 
-    def classification_score(self,feat_emb,ge2e_emb,label,length):
+        attention_mask = self.get_attention_mask(lstm_output,length) ##can only attend to hidden state that really exist
+        adder = (1.0 - attention_mask.long()) * -10000.0  ##-infty, [batchsize,150]
+        adder = torch.unsqueeze(adder,axis = 1).cuda()
+        attention_scores += adder
+
+        m = nn.Softmax(dim=2)
+        attention_probs = m(attention_scores)
+        context_layer = torch.matmul(attention_probs, V)
+        return context_layer.squeeze()
+
+
+    def classification_score(self,feat_emb,ge2e_emb,mfcc_emb,label,length,mfcc_length):
         
+        ########## allosaurus features #############
         feat_emb = feat_emb.permute(0, 2, 1)
         x = self.conv1d_1(feat_emb)
         x1 = self.conv1d_2(feat_emb)
@@ -77,13 +108,29 @@ class SER_MODEL(nn.Module):
         x = torch.add(x,x1)
         x,_ = self.lstm(x)
         x = self.adaptor0(x)
-        
-        x = self.self_attention_layer(x,length)
-        x = self.adaptor(x)
-        
-        ge2e_emb = ge2e_emb.squeeze()
-        x = torch.add(x,ge2e_emb)
+        allo_hidden_state = self.self_attention_layer(x,length) ##[bz,128]
 
+        ################## mfcc features ####################
+        mfcc = mfcc_emb.permute(0, 2, 1)
+        mfcc_x = self.conv1d_1_mfcc(mfcc)
+        mfcc_x1 = self.conv1d_2_mfcc(mfcc)
+        mfcc_x = mfcc_x.permute(0,2,1)
+        mfcc_x1 = mfcc_x1.permute(0,2,1)
+        mfcc_x = torch.add(mfcc_x,mfcc_x1)
+        mfcc_x,_ = self.lstm_mfcc(mfcc_x)
+        mfcc_hidden_state = self.self_attention_layer_mfcc(mfcc_x,mfcc_length) ##[bz,32]
+        
+        ############### combine mfcc and allosaurus ###########
+        if self.args.MFCC:
+            mfcc_allo_hidden_state = torch.add(allo_hidden_state,mfcc_hidden_state)
+            mfcc_allo_hidden_state = self.adaptor(mfcc_allo_hidden_state)
+        else:
+            mfcc_allo_hidden_state = self.adaptor1(allo_hidden_state)
+        ############### add ge2e #####################
+        ge2e_emb = ge2e_emb.squeeze()
+        x = torch.add(mfcc_allo_hidden_state,ge2e_emb)
+
+        ############## MLP ################
         x = self.dense1(x)
         x = self.bn(x)
         x = torch.tanh(x)
--- allosaurus+CNN/SER/training/utils.py
+++ allosaurus+CNN/SER/training/utils.py
@@ -4,13 +4,16 @@ import random
 import pickle as pkl
 import numpy as np
 import pandas as pd
+import librosa
 from SER.utils.runs import Run
 from SER.utils.utils import save_checkpoint
 from SER.parameters import SAVED_CHECKPOINTS
 
 MAX_LEN = 200
+WAV_PATH = "/data1/jiayu_xiao/project/wzh/data/path_to_wavs/"
 ALLO_EMB_PATH = "/data1/jiayu_xiao/project/wzh/data/allo_embedding/"
 GE2E_EMB_PATH = "../data/GE2E/"
+MFCC_EMB_PATH = "/data1/jiayu_xiao/project/wzh/data/mfcc_embeddings/"
 
 def print_progress(scores):
     positive_avg, negative_avg = round(scores[:, 0].mean().item(), 2), round(scores[:, 1].mean().item(), 2)
@@ -48,13 +51,15 @@ def tensorize_triples(args,audio_files, labels, bsize): ##transform sentence int
     length = []
     for file in audio_files:
         allo_emb = pkl.load(open(ALLO_EMB_PATH+file.split("/")[-1]+".pkl","rb"))
-        if allo_emb.shape[1] >= MAX_LEN:
-            allo_emb = allo_emb[0,:MAX_LEN,:]
+        allo_emb = allo_emb.squeeze()
+
+        if allo_emb.shape[0] >= MAX_LEN:
+            allo_emb = allo_emb[:MAX_LEN,:]
             length.append(MAX_LEN-1)
         else:
-            length.append(allo_emb.shape[1]-1)
-            zero = torch.zeros((MAX_LEN-allo_emb.shape[1], allo_emb.shape[2]))
-            allo_emb = torch.cat((allo_emb[0], zero), dim=0)
+            length.append(allo_emb.shape[0]-1)
+            zero = torch.zeros((MAX_LEN-allo_emb.shape[0], allo_emb.shape[1]))
+            allo_emb = torch.cat((allo_emb, zero), dim=0)
             
         allo_embs.append(allo_emb)
     allosaurus_embedding = torch.stack(allo_embs,axis = 0) ##len,emb_size
@@ -69,21 +74,35 @@ def tensorize_triples(args,audio_files, labels, bsize): ##transform sentence int
             ge2e_emb = torch.zeros((1,256))
         GE2E_embs.append(ge2e_emb)
     GE2E_embedding = torch.stack(GE2E_embs,axis = 0)
-    print(GE2E_embedding.shape)
+
+    mfcc_embs = []
+    mfcc_length = []
+    for file in audio_files:
+        mfccs = pkl.load(open(MFCC_EMB_PATH+file.split("/")[-1]+".pkl","rb"))
+        mfccs = torch.Tensor(np.transpose(mfccs))
+        mfcc_length.append(min(mfccs.shape[0],MAX_LEN-1))
+        if mfccs.shape[0] >= MAX_LEN:
+            mfcc_emb = mfccs[:MAX_LEN,:]
+        else:
+            zero = torch.zeros((MAX_LEN-mfccs.shape[0], mfccs.shape[1]))
+            mfcc_emb = torch.cat((mfccs, zero), dim=0)
+            
+        mfcc_embs.append(mfcc_emb)
+    mfcc_embedding = torch.stack(mfcc_embs,axis = 0) ##len,emb_size
     
-    query_batches = _split_into_batches(allosaurus_embedding,GE2E_embedding,labels,length,bsize)
+    query_batches = _split_into_batches(allosaurus_embedding,GE2E_embedding,mfcc_embedding,labels,length,mfcc_length,bsize)
     batches = []
-    for (embed,GE2E, label,length) in query_batches:
-        Q = (embed,GE2E,label,length)
+    for (embed,GE2E, MFCC,label,length,mfcc_length) in query_batches:
+        Q = (embed,GE2E,MFCC,label,length,mfcc_length)
         batches.append(Q)
 
     return batches
 
 
-def _split_into_batches(allosaurus_embedding, GE2E_embedding, labels,length,bsize):
+def _split_into_batches(allosaurus_embedding, GE2E_embedding,mfcc_embedding, labels,length,mfcc_length,bsize):
     batches = []
     for offset in range(0, allosaurus_embedding.shape[0], bsize):
-        batches.append((allosaurus_embedding[offset:offset+bsize], GE2E_embedding[offset:offset+bsize],labels[offset:offset+bsize],length[offset:offset+bsize]))
+        batches.append((allosaurus_embedding[offset:offset+bsize], GE2E_embedding[offset:offset+bsize],mfcc_embedding[offset:offset+bsize],labels[offset:offset+bsize],length[offset:offset+bsize],mfcc_length[offset:offset+bsize]))
     return batches
 
 
