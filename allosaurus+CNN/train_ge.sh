d92a95b702e430e5f9ff1f86266656edde1d674f
hannawong <zh-wang17@mails.tsinghua.edu.cn>
2022-03-16 23:00:40 +0000

    update

--- allosaurus+CNN/train_ge.sh
+++ allosaurus+CNN/train_ge.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES="1" \
 python -m \
 SER.train --bsize 32 --accum 1 --lr 0.001 --MFCC --GE2E \
---triples /data1/jiayu_xiao/project/wzh_recommendation/Speech-Emotion-Recognition/allosaurus+CNN/emodb/total.csv \
+--triples /content/drive/MyDrive/Speech-Emotion-Recognition/allosaurus+CNN/emodb/total.csv \
 --langs "ge" \
---root /data1/jiayu_xiao/project/wzh_recommendation/Speech-Emotion-Recognition/allosaurus+CNN --experiment MSMARCO-psg --run msmarco.psg.rronly --maxsteps 20000
+--root /content/drive/MyDrive/Speech-Emotion-Recognition/allosaurus+CNN/ --experiment MSMARCO-psg --run msmarco.psg.rronly --maxsteps 20000
--- allosaurus+CNN/train_pe.sh
+++ allosaurus+CNN/train_pe.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICES="1" \
+CUDA_VISIBLE_DEVICES="0" \
 python -m \
 SER.train --bsize 256 --accum 1 --lr 0.001 --MFCC --GE2E \
---triples /data1/jiayu_xiao/project/wzh_recommendation/Speech-Emotion-Recognition/allosaurus+CNN/persian/total.csv \
+--triples /content/drive/MyDrive/Speech-Emotion-Recognition/allosaurus+CNN/persian/total.csv \
 --langs "pe" \
---root /data1/jiayu_xiao/project/wzh_recommendation/Speech-Emotion-Recognition/allosaurus+CNN --experiment MSMARCO-psg --run msmarco.psg.rronly --maxsteps 20000
+--root /content/drive/MyDrive/Speech-Emotion-Recognition/allosaurus+CNN/ --experiment MSMARCO-psg --run msmarco.psg.rronly --maxsteps 20000
